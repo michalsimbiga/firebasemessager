b6369c739cf0934209f0e77eb9d00c57bdefcdf0
Michal Simbiga <michal.simbiga@gmail.com>
2020-02-25 15:03:56 +0100

    Fix linter errors

--- app/src/main/java/com/application/ui/messages/MessagesFragment.kt
+++ app/src/main/java/com/application/ui/messages/MessagesFragment.kt
@@ -1,7 +1,35 @@
 package com.application.ui.messages
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.application.databinding.FragmentMessagesBinding
+import com.application.vm.AssistedViewModelFactory
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
-class MessagesFragment: DaggerFragment() {
+class MessagesFragment : DaggerFragment() {
 
+    @Inject
+    lateinit var savedStateVmFactory: AssistedViewModelFactory
+
+    private val viewModel: MessagesViewModel by viewModels { savedStateVmFactory }
+
+    private lateinit var binding: FragmentMessagesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMessagesBinding.inflate(inflater, container, false)
+
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+            return root
+        }
+    }
 }
--- app/src/main/java/com/application/vm/MyViewModelFactory.kt
+++ app/src/main/java/com/application/vm/MyViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Provider
 
+@Suppress("UNCHECKED_CAST")
 class MyViewModelFactory(private val providers: Map<Class<out ViewModel>, Provider<ViewModel>>)
     : ViewModelProvider.Factory {
 
