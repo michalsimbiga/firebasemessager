aa33d9c4f51e3061fd683a4b5ce577bd01b6f722
Michal Simbiga <michal.simbiga@gmail.com>
2020-03-03 10:22:18 +0100

    Add chat recycler item holder

--- app/src/main/java/com/application/ui/messages/chat/ChatRecyclerViewAdapter.kt
+++ app/src/main/java/com/application/ui/messages/chat/ChatRecyclerViewAdapter.kt
@@ -2,6 +2,9 @@ package com.application.ui.messages.chat
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.application.databinding.FragmentChatRecyclerFromItemBinding
+import com.application.model.User
+import com.bumptech.glide.Glide
 
 class ChatRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -16,6 +19,18 @@ class ChatRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+}
+
+class ChatFromViewHolder(private val binding: FragmentChatRecyclerFromItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(user: User) = with(binding) {
+        Glide.with(root.context)
+            .load(user.profileImage)
+            .centerCrop()
+            .into(fragmentChatFromPicture)
+    }
+}
 
 //    class MyChatMessageViewHolder(private val binding: FragmentNewMessageRecyclerItemBinding) :
 //        RecyclerView.ViewHolder(binding.root) {
@@ -32,4 +47,3 @@ class ChatRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
 //                userItemName.text = user.username
 //                userItemMainLayout.setOnClickListener { onClickCallback?.invoke(user) }
 //            }
-}
