0cd9420129790ab36567a82cdeda26ee185b9e97
Michal Simbiga <michal.simbiga@gmail.com>
2020-03-11 17:09:08 +0100

    Add SavedStateHandle extension for live data

--- app/src/main/java/com/application/domain/extensions/StateHandleExtenstions.kt
+++ app/src/main/java/com/application/domain/extensions/StateHandleExtenstions.kt
@@ -1,5 +1,6 @@
 package com.application.domain.extensions
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import timber.log.Timber
 import kotlin.properties.ReadWriteProperty
@@ -14,10 +15,10 @@ fun <T> SavedStateHandle.delegate(defaultVal: T) = object : ReadWriteProperty<An
         set(property.name, value).also { Timber.i("TESTING setValue $value")}
 }
 
-fun <T> SavedStateHandle.liveData(defaultVal: T) = object : ReadWriteProperty<Any, T> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): T =
-        getLiveData(property.name) ?: defaultVal
+fun <T> SavedStateHandle.liveData(defaultVal: T) = object : ReadWriteProperty<Any, MutableLiveData<T> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): MutableLiveData<T> =
+        getLiveData(property.name, defaultVal)
 
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) =
-        set(property.name, value).also { Timber.i("TESTING setValue $value")}
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: MutableLiveData<T>) =
+        set(property.name, value)
 }
